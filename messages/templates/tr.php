bbc2abcae39de5975df40b7f9ce98d9ec2f3eea7
cniska@live.com <cniska@live.com@a36fc3c5-a870-cc65-72ee-e9cbbe3c4062>
2010-07-26 09:58:51 +0000

    Added template for translation
    git-svn-id: http://yii-rights.googlecode.com/svn/trunk@16 a36fc3c5-a870-cc65-72ee-e9cbbe3c4062

---
+++ messages/templates/tr.php
@@ -0,0 +1,89 @@
+<?php
+/**
+* Finnish translations for Rights.
+*
+* Please contact me if you find an error in the translation.
+*
+* @author Christoffer Niska <cniska@live.com>
+* @copyright Copyright &copy; 2010 Christoffer Niska
+* @since 0.9.1
+*/
+return array(
+	/**
+	* components/Rights.php
+	*/
+	'operation'=>'',
+	'task'=>'',
+	'role'=>'',
+	/**
+	* components/RightsFilter.php
+	*/
+	'You are not authorized to perform this action.'=>'',
+	/**
+	* views/main/permissions.php
+	*/
+	'Permissions'=>'',
+	'Description'=>'',
+	'Revoke'=>'',
+	'Inherited'=>'',
+	'Assign'=>'',
+	'Hover to see from where the permission is inherited.'=>'',
+	'Parents'=>'',
+	'No auth items found.'=>'',
+	/**
+	* views/main/operations.php
+	*/
+	'Operations'=>'',
+	'Name'=>'',
+	'Business rule'=>'',
+	'Data'=>'',
+	'Delete'=>'',
+	'Are you sure to delete this operation?'=>'',
+	'Values within square brackets tell how many children each item has.'=>'',
+	'No operations found.'=>'',
+	/**
+	* views/main/tasks.php
+	*/
+	'Tasks'=>'',
+	'Are you sure to delete this task?'=>'',
+	'No tasks found.'=>'',
+	/**
+	* views/main/roles.php
+	*/
+	'Roles'=>'',
+	'Are you sure to delete this role?'=>'',
+	'No roles found.'=>'',
+	/**
+	* views/authItem/authItemForm.php
+	*/
+	'Save'=>'',
+	/**
+	* views/authItem/create.php
+	*/
+	'Create Auth Item'=>'',
+	'Notice: Auth Item type cannot be changed afterwards.'=>'',
+	/**
+	* views/authItem/update.php
+	*/
+	'Auth Item'=>'',
+	'Update :name'=>'',
+	'This item has no parents.'=>'',
+	'Children'=>'',
+	'Remove'=>'',
+	'Are you sure you want to remove this child?'=>'',
+	'This item has no children.'=>'',
+	'Add Child'=>'',
+	/**
+	* views/assignment/user.php
+	*/
+	'Assignments for :username'=>'',
+	'Are you sure to revoke this assignment?'=>'',
+	'This user has not been assigned any auth items.'=>'',
+	'Add Assignment'=>'',
+	/**
+	* views/assignments/view.php
+	*/
+	'Assignments'=>'',
+	'Username'=>'',
+	'No users found.'=>'',
+);
